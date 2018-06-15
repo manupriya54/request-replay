78104e250d25f29c9f9b0d92cca1849c7c8960cc
Tom Macie <tm@stripe.com>
2018-06-15 09:41:10 -0700

    Fix C# deserialization

--- csharp/RequestReplay/MyLog.cs
+++ csharp/RequestReplay/MyLog.cs
@@ -2,19 +2,20 @@ using System.Collections.Generic;
 
 public class MyLog
 {
-  public MyRequest request { get; }
-  public MyResponse response { get; }
+  public MyRequest request { get; set; }
+  public MyResponse response { get; set; }
 }
 
 public class MyResponse
 {
-  public string body { get; }
-  public int code { get; }
+  public string body { get; set; }
+  public int code { get; set; }
 }
 
 public class MyRequest
 {
-  public string url { get; }
-  public Dictionary<string, string> headers { get; }
-  public string body { get; }
+  public string url { get; set; }
+  public string method { get; set; }
+  public Dictionary<string, string> headers { get; set; }
+  public string body { get; set; }
 }
